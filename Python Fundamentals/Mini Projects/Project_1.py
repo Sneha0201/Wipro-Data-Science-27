0bc449cd98b7ed35d1e3379a9a429b0267819b52
Sneha0201 <snehachaurasia0201@gmail.com>
2026-06-28 21:51:17 +0530

    Create Project_1.py

---
+++ Python Fundamentals/Mini Projects/Project_1.py
@@ -0,0 +1,7 @@
+distance = int(input("How far you have to travel in miles? :"))
+if distance < 3:
+    print("I suggest ride a bicycle for your destination.")
+elif 3 <= distance < 300:
+    print("I suggest ride a motorcycle for your destination.")
+else:
+    print("I suggest ride a super-car for your destination.")
