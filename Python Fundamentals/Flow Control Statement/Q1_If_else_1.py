5157e8d538041590a282939e8bba5f2ac5641262
Sneha0201 <snehachaurasia0201@gmail.com>
2026-06-30 12:57:42 +0530

    Create Q1_If_else_1.py

---
+++ Python Fundamentals/Flow Control Statement/Q1_If_else_1.py
@@ -0,0 +1,9 @@
+#Check if number is negative, positive or zero.
+
+n = int(input("Enter a number: "))
+if n < 0:
+    print("Number is Negative")
+elif n == 0:
+    print("Number is Zero")
+else:
+    print("Number is Positive")
