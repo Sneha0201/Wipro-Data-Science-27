ef9df5c033fbb94c3e5f72a9786357fda5c0cf24
Sneha0201 <snehachaurasia0201@gmail.com>
2026-06-30 12:57:49 +0530

    Delete If_else_1.py

--- Python Fundamentals/Flow Control Statement/If_else_1.py
+++
@@ -1,9 +0,0 @@
-#Check if number is negative, positive or zero.
-
-n = int(input("Enter a number: "))
-if n < 0:
-    print("Number is Negative")
-elif n == 0:
-    print("Number is Zero")
-else:
-    print("Number is Positive")
