5ce220e40ae5f67f6f05d6a0627f1ffb69492d60
Sneha0201 <snehachaurasia0201@gmail.com>
2026-06-30 12:59:05 +0530

    Delete If_else_3.py

--- Python Fundamentals/Flow Control Statement/If_else_3.py
+++
@@ -1,9 +0,0 @@
-#Check if two non-negative number have same last digit.
-
-num1 = int(input("Enter first no.: "))
-num2 = int(input("Enter second no.: "))
-
-if num1 % 10 == num2 % 10:
-    print("True")
-else:
-    print("False")
