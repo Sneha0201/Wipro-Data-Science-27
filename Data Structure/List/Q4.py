930bd7aa73527b1ebef39e827cc3e7b929112683
Sneha0201 <snehachaurasia0201@gmail.com>
2026-07-01 13:27:12 +0530

    Create Q4.py

---
+++ Data Structure/List/Q4.py
@@ -0,0 +1,6 @@
+# WAP to print the no. of occurences of a specified element in a list
+
+list = [10,60,50,30,50,20,70,20,10,20]
+element = 20
+count = list.count(element)
+print(f"No. of occurence of {element}: ", count)
