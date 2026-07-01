07a98e35b2535d06dcbba925e620c079c3542f72
Sneha0201 <snehachaurasia0201@gmail.com>
2026-07-01 13:41:44 +0530

    Create Q5.py

---
+++ Data Structure/List/Q5.py
@@ -0,0 +1,10 @@
+# WAP to to append the items of list1 to list2in the front
+
+list1 = [1,2,3]
+list2 = [4,5,6]
+
+print("List1:", list1)
+print("List2:", list2)
+
+result = list1 + list2
+print("Result after appending list1 to list2:", result)
