36a1fc73ed0fecf0b81edb3d9bd3efaba5fcf778
Sneha0201 <snehachaurasia0201@gmail.com>
2026-07-01 14:55:53 +0530

    Create Q3.py

---
+++ Data Structure/Dictionary/Q3.py
@@ -0,0 +1,8 @@
+# WAP to check if the given key already exist in a dictionary
+
+dict = {'a':10, 'b':20, 'c':30, 'd':40}
+key = "b"
+if key in dict:
+    print(f"Key {key} already exist in dictionary")
+else:
+    print(f"Key {key} does not exist in dictionary")
